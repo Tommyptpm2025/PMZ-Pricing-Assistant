1f6d9bb1c8814ec9deeaf2e46990ac41136e553e
Tom Peterson <tom@totalprofitmanagement.com>
2026-06-02 21:26:55 -0600

    Added Salesperson field + fixed theme provider keyboard crash

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -47,8 +47,9 @@ function ThemeHotkey() {
         return
       }
 
-      if (event.key.toLowerCase() !== "d") {
-        return
+      const key = event?.key?.toLowerCase?.();
+      if (key !== "d") {
+        return;
       }
 
       if (isTypingTarget(event.target)) {
